bec07979439e64a8d05e179bc4424bf042582a73
Noam <nbenari@gmail.com>
2011-01-29 03:44:02 +0200

    renaming a method

--- lib/sorcery/controller/submodules/session_timeout.rb
+++ lib/sorcery/controller/submodules/session_timeout.rb
@@ -25,9 +25,9 @@ module Sorcery
           end
           
           # To be used as a before_filter, before authenticate
-          def timeout_session
+          def validate_session
             session_to_use = Config.session_timeout_from_last_action ? session[:last_action_time] : session[:login_time]
-            if session_to_use && Time.now.utc - session_to_use > Config.session_timeout
+            if session_to_use && (Time.now.utc - session_to_use > Config.session_timeout)
               reset_session
               @logged_in_user = false
             end
