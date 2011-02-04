930e224ad48c813a97060561e783a26f21c5220c
Noam Ben Ari <nbenari@gmail.com>
2011-02-05 00:05:53 +0200

    adding HTTP BASIC AUTH submodule with tests

--- lib/sorcery/controller/submodules/http_basic_auth.rb
+++ lib/sorcery/controller/submodules/http_basic_auth.rb
@@ -1,7 +1,7 @@
 module Sorcery
   module Controller
     module Submodules
-      module HTTPBasicAuth
+      module HttpBasicAuth
         def self.included(base)
           base.send(:include, InstanceMethods)
           Config.login_sources << :login_from_basic_auth
@@ -9,13 +9,26 @@ module Sorcery
         
         module InstanceMethods
 
+          # to be used as a before_filter.
+          # The method sets a session when requesting the user's credentials.
+          # This is a trick to overcome the way HTTP authentication work (explained below):
+          #
+          # Once the user fills the credentials once, the browser will always send it to the server when visiting the website, until the browser is closed.
+          # This causes wierd behaviour if the user logs out. The session is reset, yet the user is re-logged in by the before_filter calling 'login_from_basic_auth'.
+          # To overcome this, we set a session when requesting the password, which logout will reset, and that's how we know if we need to request for HTTP auth again.
+          def require_user_login_from_http
+            request_http_basic_authentication and (session[:http_authentication_used] = true) and return if request.authorization.nil? || session[:http_authentication_used].nil?
+            require_user_login
+          end
+          
           protected
           
           def login_from_basic_auth
             authenticate_with_http_basic do |username, password|
-              Config.user_class.authenticate(username, password)
+              @logged_in_user = (Config.user_class.authenticate(username, password) if session[:http_authentication_used]) || false
             end
           end
+          
         end
 
       end
