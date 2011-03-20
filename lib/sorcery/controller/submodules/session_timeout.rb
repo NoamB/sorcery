a74cfc5207434254d72ccee3e520e5f0bd598dd6
Noam Ben Ari <nbenari@gmail.com>
2011-03-20 23:13:52 +0200

    removed prints

--- lib/sorcery/controller/submodules/session_timeout.rb
+++ lib/sorcery/controller/submodules/session_timeout.rb
@@ -35,10 +35,6 @@ module Sorcery
           # To be used as a before_filter, before require_login
           def validate_session
             session_to_use = Config.session_timeout_from_last_action ? session[:last_action_time] : session[:login_time]
-            p Time.now.utc - session_to_use if session_to_use
-            p Time.now.utc
-            p session_to_use
-            p session[:last_action_time]
             if session_to_use && (Time.now.utc - session_to_use > Config.session_timeout)
               reset_session
               @current_user = false
