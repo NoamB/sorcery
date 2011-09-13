eaa2fd7221bd1015530c5c20b655c81d4fc2b4aa
Erlingur Þorsteinsson <erlingur@themoon.is>
2011-09-13 02:55:51 +0000

    Add the logged_in? method as a helper for use in the view layer. (Issue #21).
    Haven't managed to test this unfortunately so take this with a grain of salt :)

--- lib/sorcery/engine.rb
+++ lib/sorcery/engine.rb
@@ -10,6 +10,7 @@ module Sorcery
     initializer "extend Controller with sorcery" do |app|
       ActionController::Base.send(:include, Sorcery::Controller)
       ActionController::Base.helper_method :current_user
+      ActionController::Base.helper_method :logged_in?
     end
     
     rake_tasks do
