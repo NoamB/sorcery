1941e4c8530b741d18c94074e28f645ad721c8a2
Noam Ben Ari <nbenari@gmail.com>
2011-09-15 22:39:13 +0300

    typo

--- lib/generators/sorcery_migration/templates/core.rb
+++ lib/generators/sorcery_migration/templates/core.rb
@@ -1,7 +1,7 @@
 class SorceryCore < ActiveRecord::Migration
   def self.up
     create_table :users do |t|
-      t.string :username,         :null => false  # if you use another filed as a username, for example email, you can safely remove this field.
+      t.string :username,         :null => false  # if you use another field as a username, for example email, you can safely remove this field.
       t.string :email,            :default => nil # if you use this field as a username, you might want to make it :null => false.
       t.string :crypted_password, :default => nil
       t.string :salt,             :default => nil
