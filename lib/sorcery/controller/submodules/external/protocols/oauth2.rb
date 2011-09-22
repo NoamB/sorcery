fba4a6ecc3fb03058cc4f68de2ba1b15e7babff7
WillyWG <willywg@gmail.com>
2011-09-22 14:54:11 -0500

    Add configurable ca_path for oauth2

--- lib/sorcery/controller/submodules/external/protocols/oauth2.rb
+++ lib/sorcery/controller/submodules/external/protocols/oauth2.rb
@@ -5,18 +5,33 @@ module Sorcery
       module External
         module Protocols
           module Oauth2
+            def self.included(base)
+              base.send(:include, InstanceMethods)
+              Config.module_eval do
+                class << self
+                  attr_accessor :ca_file                     # how long in seconds to keep the session alive.
+                                
+                  def merge_oauth2_defaults!
+                    @defaults.merge!(:@ca_file => File.join(File.expand_path(File.dirname(__FILE__)), 
+    'certs/ca-bundle.crt'))
+                  end
+                end
+                merge_oauth2_defaults!
+              end
+            end
+            
             def oauth_version
               "2.0"
             end
           
             def authorize_url(*args)
-              client = ::OAuth2::Client.new(@key, @secret, :site => @site)
-              client.web_server.authorize_url(:redirect_uri => @callback_url, :scope => @scope, :ssl => { :ca_file => File.join(File.expand_path(File.dirname(__FILE__)), 'certs/ca-bundle.crt')})
+              client = ::OAuth2::Client.new(@key, @secret, :site => @site, :ssl => { :ca_file => Config.ca_file })
+              client.web_server.authorize_url(:redirect_uri => @callback_url, :scope => @scope)
             end
           
             def get_access_token(args)
-              client.web_server.authorize_url(:redirect_uri => @callback_url, :scope => @scope, :ssl => { :ca_file => File.join(File.expand_path(File.dirname(__FILE__)), 'certs/ca-bundle.crt')})
-              client.web_server.get_access_token(args[:code], :redirect_uri => @callback_url)
+              client = ::OAuth2::Client.new(@key, @secret, :site => @site, :ssl => { :ca_file => Config.ca_file })
+		          client.web_server.get_access_token(args[:code], :redirect_uri => @callback_url)
             end
           end
         end
