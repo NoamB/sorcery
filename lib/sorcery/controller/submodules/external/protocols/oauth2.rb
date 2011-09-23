030f5bc8bfed325eae6e6a310fabaa15f9706fc4
Bradley Schaefer <bradley@anythingsocial.com>
2011-09-23 16:14:35 -0500

    Fixing github login_url

--- lib/sorcery/controller/submodules/external/protocols/oauth2.rb
+++ lib/sorcery/controller/submodules/external/protocols/oauth2.rb
@@ -9,8 +9,8 @@ module Sorcery
               "2.0"
             end
           
-            def authorize_url(*args)
-              client = ::OAuth2::Client.new(@key, @secret, :site => @site)
+            def authorize_url(options = {})
+              client = ::OAuth2::Client.new(@key, @secret, {:site => @site}.merge!(options))
               client.web_server.authorize_url(:redirect_uri => @callback_url, :scope => @scope)
             end
           
