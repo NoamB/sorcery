c684248fe63b24bf8af02f3611e80d9e6e289c8d
James Cox <james@imaj.es>
2011-11-03 11:28:19 -0400

    whitespace again. :(

--- lib/sorcery/crypto_providers/bcrypt.rb
+++ lib/sorcery/crypto_providers/bcrypt.rb
@@ -2,9 +2,9 @@ require 'bcrypt'
 
 module Sorcery
   module CryptoProviders
-    # For most apps Sha512 is plenty secure, but if you are building an app that stores nuclear 
+    # For most apps Sha512 is plenty secure, but if you are building an app that stores nuclear
     # launch codes you might want to consier BCrypt. This is an extremely
-    # secure hashing algorithm, mainly because it is slow. 
+    # secure hashing algorithm, mainly because it is slow.
     # A brute force attack on a BCrypt encrypted password would take much longer than a brute force attack on a
     # password encrypted with a Sha algorithm. Keep in mind you are sacrificing performance by using this,
     # generating a password takes exponentially longer than any
@@ -40,29 +40,29 @@ module Sorcery
     # You are good to go!
     class BCrypt
       class << self
-        # This is the :cost option for the BCrpyt library. 
+        # This is the :cost option for the BCrpyt library.
         # The higher the cost the more secure it is and the longer is take the generate a hash. By default this is 10.
-        # Set this to whatever you want, play around with it to get that perfect balance between 
+        # Set this to whatever you want, play around with it to get that perfect balance between
         # security and performance.
         def cost
           @cost ||= 10
         end
         attr_writer :cost
         alias :stretches= :cost=
-        
+
         # Creates a BCrypt hash for the password passed.
         def encrypt(*tokens)
           ::BCrypt::Password.create(join_tokens(tokens), :cost => cost)
         end
-        
+
         # Does the hash match the tokens? Uses the same tokens that were used to encrypt.
         def matches?(hash, *tokens)
           hash = new_from_hash(hash)
           return false if hash.nil? || hash == {}
           hash == join_tokens(tokens)
         end
-        
-        # This method is used as a flag to tell Sorcery to "resave" the password 
+
+        # This method is used as a flag to tell Sorcery to "resave" the password
         # upon a successful login, using the new cost
         def cost_matches?(hash)
           hash = new_from_hash(hash)
@@ -72,17 +72,17 @@ module Sorcery
             hash.cost == cost
           end
         end
-        
+
         def reset!
           @cost = 10
         end
-        
+
         private
-        
+
         def join_tokens(tokens)
           tokens.flatten.join
         end
-        
+
         def new_from_hash(hash)
           begin
             ::BCrypt::Password.new(hash)
