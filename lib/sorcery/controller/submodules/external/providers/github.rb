90ea886bb2179737ea039f6910435330f1288f89
Marc Lainez <marc.lainez@gmail.com>
2011-11-07 23:47:50 +0100

    Fix github Oauth user info endpoint and clean up

--- lib/sorcery/controller/submodules/external/providers/github.rb
+++ lib/sorcery/controller/submodules/external/providers/github.rb
@@ -31,6 +31,8 @@ module Sorcery
                 attr_accessor :key,
                               :secret,
                               :callback_url,
+                              :auth_path,
+                              :token_path,
                               :site,
                               :user_info_path,
                               :user_info_mapping
@@ -39,7 +41,9 @@ module Sorcery
 
                 def init
                   @site           = "https://github.com/"
-                  @user_info_path = "/api/v2/json/user/show"
+                  @user_info_path = "https://api.github.com/user"
+                  @auth_path      = "/login/oauth/authorize"
+                  @token_path     = "/login/oauth/access_token"
                   @user_info_mapping = {}
                 end
 
@@ -58,14 +62,18 @@ module Sorcery
                 # calculates and returns the url to which the user should be redirected,
                 # to get authenticated at the external provider's site.
                 def login_url(params,session)
-                  self.authorize_url({:authorize_path => '/login/oauth/authorize'})
+                  self.authorize_url({:authorize_path => @auth_path})
                 end
 
                 # tries to login the user from access token
                 def process_callback(params,session)
                   args = {}
                   args.merge!({:code => params[:code]}) if params[:code]
-                  @access_token = self.get_access_token(args)
+                  options = {
+                    :access_token_path => @token_path,
+                    :access_token_method => :post
+                  }
+                  @access_token = self.get_access_token(args, options)
                 end
 
               end
