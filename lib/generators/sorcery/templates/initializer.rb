a32a5f8c78ad8e113d3d9ba363d691d5927f6739
Kurtis Rainbolt-Greene <me+github@kurtisrainboltgreene.name>
2012-02-09 13:21:47 -0600

    Adding default values, removing default values from expressions

--- lib/generators/sorcery/templates/initializer.rb
+++ lib/generators/sorcery/templates/initializer.rb
@@ -8,58 +8,68 @@ Rails.application.config.sorcery.submodules = []
 Rails.application.config.sorcery.configure do |config|
   # -- core --
   # What controller action to call for non-authenticated users. You can also
-  # override 'not_authenticated' instead
-  # config.not_authenticated_action = :not_authenticated
-
+  # override the 'not_authenticated' method of course.
+  # Default: `:not_authenticated`
+  # config.not_authenticated_action =
 
   # When a non logged in user tries to enter a page that requires login, save
   # the URL he wanted to reach, and send him there after login, using 'redirect_back_or_to'.
-  # config.save_return_to_url = true
+  # Default: `true`
+  # config.save_return_to_url =
 
 
   # Set domain option for cookies; Useful for remember_me submodule.
-  # config.cookie_domain = nil
+  # Default: `nil`
+  # config.cookie_domain =
 
   # -- session timeout --
   # How long in seconds to keep the session alive.
-  # config.session_timeout = 3600
+  # Default: `3600`
+  # config.session_timeout =
 
   # Use the last action as the beginning of session timeout.
-  # config.session_timeout_from_last_action = false
+  # Default: `false`
+  # config.session_timeout_from_last_action =
 
   # -- http_basic_auth --
   # What realm to display for which controller name. For example {"My App" => "Application"}
-  # config.controller_to_realm_map = {"application" => "Application"}
+  # Default: `{"application" => "Application"}`
+  # config.controller_to_realm_map =
 
   # -- activity logging --
   # will register the time of last user login, every login.
-  # config.register_login_time = true
+  # Default: `true`
+  # config.register_login_time =
 
   # will register the time of last user logout, every logout.
-  # config.register_logout_time = true
+  # Default: `true`
+  # config.register_logout_time =
 
   # will register the time of last user action, every action.
-  # config.register_last_activity_time = true
+  # Default: `true`
+  # config.register_last_activity_time =
 
   # -- external --
   # What providers are supported by this app, i.e. [:twitter, :facebook, :github, :google, :liveid] .
-  # config.external_providers = []
+  # Default: `[]`
+  # config.external_providers =
 
   # You can change it by your local ca_file. i.e. '/etc/pki/tls/certs/ca-bundle.crt'
   # Path to ca_file. By default use a internal ca-bundle.crt.
-  # config.ca_file = 'path/to/ca_file'
+  # Default: `'path/to/ca_file'`
+  # config.ca_file =
 
 
   # Twitter wil not accept any requests nor redirect uri containing localhost,
   # make sure you use 0.0.0.0:3000 to access your app in development
   #
-  # config.twitter.key = "eYVNBjBDi33aa9GkA3w"
-  # config.twitter.secret = "XpbeSdCoaKSmQGSeokz5qcUATClRW5u08QWNfv71N8"
+  # config.twitter.key = ""
+  # config.twitter.secret = ""
   # config.twitter.callback_url = "http://0.0.0.0:3000/oauth/callback?provider=twitter"
   # config.twitter.user_info_mapping = {:email => "screen_name"}
   #
-  # config.facebook.key = "34cebc81c08a521bc66e212f947d73ec"
-  # config.facebook.secret = "5b458d179f61d4f036ee66a497ffbcd0"
+  # config.facebook.key = ""
+  # config.facebook.secret = ""
   # config.facebook.callback_url = "http://0.0.0.0:3000/oauth/callback?provider=facebook"
   # config.facebook.user_info_mapping = {:email => "name"}
   #
@@ -68,8 +78,8 @@ Rails.application.config.sorcery.configure do |config|
   # config.github.callback_url = "http://0.0.0.0:3000/oauth/callback?provider=github"
   # config.github.user_info_mapping = {:email => "name"}
   #
-  # config.google.key = "491253340633.apps.googleusercontent.com"
-  # config.google.secret = "4oE6kXqbL_LN-VGcGcg7qgdL"
+  # config.google.key = ""
+  # config.google.secret = ""
   # config.google.callback_url = "http://0.0.0.0:3000/oauth/callback?provider=google"
   # config.google.user_info_mapping = {:email => "email", :username => "name"}
   #
@@ -86,133 +96,172 @@ Rails.application.config.sorcery.configure do |config|
   config.user_config do |user|
     # -- core --
     # specify username attributes, for example: [:username, :email].
-    # user.username_attribute_names = [:username]
+    # Default: `[:username]`
+    # user.username_attribute_names =
 
     # change *virtual* password attribute, the one which is used until an encrypted one is generated.
-    # user.password_attribute_name = :password
+    # Default: `:password`
+    # user.password_attribute_name =
 
     # downcase the username before trying to authenticate, default is false
-    # user.downcase_username_before_authenticating = false
+    # Default: `false`
+    # user.downcase_username_before_authenticating =
 
     # change default email attribute.
-    # user.email_attribute_name = :email
+    # Default: `:email`
+    # user.email_attribute_name =
 
     # change default crypted_password attribute.
-    # user.crypted_password_attribute_name =  :crypted_password
+    # Default: `:crypted_password`
+    # user.crypted_password_attribute_name =
 
     # what pattern to use to join the password with the salt
-    # user.salt_join_token = ""
+    # Default: `""`
+    # user.salt_join_token =
 
     # change default salt attribute.
-    # user.salt_attribute_name = :salt
+    # Default: `:salt`
+    # user.salt_attribute_name =
 
     # how many times to apply encryption to the password.
-    # user.stretches = nil
+    # Default: `nil`
+    # user.stretches =
 
 
     # encryption key used to encrypt reversible encryptions such as AES256.
     #
     # WARNING: If used for users' passwords, changing this key will leave passwords undecryptable!
-    # user.encryption_key = nil
+    # Default: `nil`
+    # user.encryption_key =
 
     # use an external encryption class.
-    # user.custom_encryption_provider = nil
+    # Default: `nil`
+    # user.custom_encryption_provider =
 
     # encryption algorithm name. See 'encryption_algorithm=' for available options.
-    # user.encryption_algorithm = :bcrypt
+    # Default: `:bcrypt`
+    # user.encryption_algorithm =
 
     # make this configuration inheritable for subclasses. Useful for ActiveRecord's STI.
-    # user.subclasses_inherit_config = false
+    # Default: `false`
+    # user.subclasses_inherit_config =
 
     # -- user_activation --
     # the attribute name to hold activation state (active/pending).
-    # user.activation_state_attribute_name = :activation_state
+    # Default: `:activation_state`
+    # user.activation_state_attribute_name =
 
     # the attribute name to hold activation code (sent by email).
-    # user.activation_token_attribute_name = :activation_token
+    # Default: `:activation_token`
+    # user.activation_token_attribute_name =
 
     # the attribute name to hold activation code expiration date.
-    # user.activation_token_expires_at_attribute_name = :activation_token_expires_at
+    # Default: `:activation_token_expires_at`
+    # user.activation_token_expires_at_attribute_name =
 
     # how many seconds before the activation code expires. nil for never expires.
-    # user.activation_token_expiration_period =  nil
+    # Default: `nil`
+    # user.activation_token_expiration_period =
 
     # your mailer class. Required.
-    # user.user_activation_mailer = nil
+    # Default: `nil`
+    # user.user_activation_mailer =
 
     # activation needed email method on your mailer class.
-    # user.activation_needed_email_method_name = :activation_needed_email
+    # Default: `:activation_needed_email`
+    # user.activation_needed_email_method_name =
 
     # activation success email method on your mailer class.
-    # user.activation_success_email_method_name = :activation_success_email
+    # Default: `:activation_success_email`
+    # user.activation_success_email_method_name =
 
     # do you want to prevent or allow users that did not activate by email to login?
-    # user.prevent_non_active_users_to_login = true
+    # Default: `true`
+    # user.prevent_non_active_users_to_login =
 
 
     # -- reset_password --
     # reset password code attribute name.
-    # user.reset_password_token_attribute_name = :reset_password_token
+    # Default: `:reset_password_token`
+    # user.reset_password_token_attribute_name =
 
     # expires at attribute name.
-    # user.reset_password_token_expires_at_attribute_name = :reset_password_token_expires_at
+    # Default: `:reset_password_token_expires_at`
+    # user.reset_password_token_expires_at_attribute_name =
 
     # when was email sent, used for hammering protection.
-    # user.reset_password_email_sent_at_attribute_name = :reset_password_email_sent_at
+    # Default: `:reset_password_email_sent_at`
+    # user.reset_password_email_sent_at_attribute_name =
 
     # mailer class. Needed.
-    # user.reset_password_mailer = nil
+    # Default: `nil`
+    # user.reset_password_mailer =
 
     # reset password email method on your mailer class.
-    # user.reset_password_email_method_name = :reset_password_email
+    # Default: `:reset_password_email`
+    # user.reset_password_email_method_name =
 
     # how many seconds before the reset request expires. nil for never expires.
-    # user.reset_password_expiration_period = nil
+    # Default: `nil`
+    # user.reset_password_expiration_period =
 
     # hammering protection, how long to wait before allowing another email to be sent.
-    # user.reset_password_time_between_emails = 5 * 60
+    # Default: `5 * 60`
+    # user.reset_password_time_between_emails =
 
 
     # -- brute_force_protection --
     # Failed logins attribute name.
-    # user.failed_logins_count_attribute_name = :failed_logins_count
+    # Default: `:failed_logins_count`
+    # user.failed_logins_count_attribute_name =
 
     # This field indicates whether user is banned and when it will be active again.
-    # user.lock_expires_at_attribute_name = :lock_expires_at
+    # Default: `:lock_expires_at`
+    # user.lock_expires_at_attribute_name =
 
     # How many failed logins allowed.
-    # user.consecutive_login_retries_amount_limit = 50
+    # Default: `50`
+    # user.consecutive_login_retries_amount_limit =
 
     # How long the user should be banned. in seconds. 0 for permanent.
-    # user.login_lock_time_period = 60 * 60
+    # Default: `60 * 60`
+    # user.login_lock_time_period =
 
 
     # -- activity logging --
     # Last login attribute name.
-    # user.last_login_at_attribute_name = :last_login_at
+    # Default: `:last_login_at`
+    # user.last_login_at_attribute_name =
 
     # Last logout attribute name.
-    # user.last_logout_at_attribute_name = :last_logout_at
+    # Default: `:last_logout_at`
+    # user.last_logout_at_attribute_name =
 
     # Last activity attribute name.
-    # user.last_activity_at_attribute_name = :last_activity_at
+    # Default: `:last_activity_at`
+    # user.last_activity_at_attribute_name =
 
     # How long since last activity is he user defined logged out?
-    # user.activity_timeout = 10 * 60
+    # Default: `10 * 60`
+    # user.activity_timeout =
 
 
     # -- external --
     # Class which holds the various external provider data for this user.
-    # user.authentications_class = nil
+    # Default: `nil`
+    # user.authentications_class =
 
     # User's identifier in authentications class.
-    # user.authentications_user_id_attribute_name = :user_id
+    # Default: `:user_id`
+    # user.authentications_user_id_attribute_name =
 
     # Provider's identifier in authentications class.
-    # user.provider_attribute_name = :provider
+    # Default: `:provider`
+    # user.provider_attribute_name =
 
     # User's external unique identifier in authentications class.
-    # user.provider_uid_attribute_name = :uid
+    # Default: `:uid`
+    # user.provider_uid_attribute_name =
   end
 
   # This line must come after the 'user config' block.
