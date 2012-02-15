66805549c3319da59c5ba6bf08696d2ad3b25bfd
Matt Yoho <mby@mattyoho.com>
2012-02-15 17:53:54 -0500

    Correctly override authorization path for GitHub
    OAuth2::Client#new expects the authorization endpoint to be specified with an :authorize_url key, not :authorize_path (though it is indeed a path and not a URL; shrug.)

--- lib/sorcery/controller/submodules/external/providers/github.rb
+++ lib/sorcery/controller/submodules/external/providers/github.rb
@@ -63,7 +63,7 @@ module Sorcery
                 # calculates and returns the url to which the user should be redirected,
                 # to get authenticated at the external provider's site.
                 def login_url(params,session)
-                  self.authorize_url({:authorize_path => @auth_path})
+                  self.authorize_url({:authorize_url => @auth_path})
                 end
 
                 # tries to login the user from access token
