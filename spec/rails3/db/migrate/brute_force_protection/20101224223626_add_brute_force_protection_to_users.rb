6253b6470b8eaacf0e87143dc4d9d4139d05c261
foohey <offim.r@gmail.com>
2012-04-02 07:49:28 +0200

    Added "Unlock token" feature

--- spec/rails3/db/migrate/brute_force_protection/20101224223626_add_brute_force_protection_to_users.rb
+++ spec/rails3/db/migrate/brute_force_protection/20101224223626_add_brute_force_protection_to_users.rb
@@ -2,6 +2,7 @@ class AddBruteForceProtectionToUsers < ActiveRecord::Migration
   def self.up
     add_column :users, :failed_logins_count, :integer, :default => 0
     add_column :users, :lock_expires_at, :datetime, :default => nil
+    add_column :users, :unlock_token, :string, :default => nil
   end
 
   def self.down
