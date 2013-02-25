a9d2521540c121171c6cdca08682c6762139df2b
Roger Campos <roger@itnig.net>
2013-02-25 17:14:24 +0100

    Add facebook display setting in the initializer

--- lib/generators/sorcery/templates/initializer.rb
+++ lib/generators/sorcery/templates/initializer.rb
@@ -114,6 +114,7 @@ Rails.application.config.sorcery.configure do |config|
   # config.facebook.callback_url = "http://0.0.0.0:3000/oauth/callback?provider=facebook"
   # config.facebook.user_info_mapping = {:email => "name"}
   # config.facebook.access_permissions = ["email", "publish_stream"]
+  # config.facebook.display = "page"
   #
   # config.github.key = ""
   # config.github.secret = ""
