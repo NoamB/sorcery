a7daf001483337b050261ceb9c2af3568c7d9e3e
Arnvald <arnvald.to@gmail.com>
2014-03-02 17:51:08 +1100

    Update integrations tests
    * http_method was added to both login_user and logout_user helpers
    * parameter name depends on name of user name attribute

--- lib/sorcery/test_helpers/rails/integration.rb
+++ lib/sorcery/test_helpers/rails/integration.rb
@@ -3,20 +3,22 @@ module Sorcery
     module Rails
       module Integration
         
-        #Accepts arguments for user to login and route to use 
-        #Defaults - @user and 'sessions_url'
-        def login_user(user = nil, route = nil)
+        #Accepts arguments for user to login, route to use and HTTP method
+        #Defaults - @user, 'sessions_url' and POST
+        def login_user(user = nil, route = nil, http_method = :post)
           user ||= @user
-          username = user.send(user.sorcery_config.username_attribute_names.first)
           route ||= sessions_url
-          page.driver.post(route, { username: username, password: 'secret' })
+
+          username_attr = user.sorcery_config.username_attribute_names.first
+          username = user.send(username_attr)
+          page.driver.send(http_method, route, { :"#{username_attr}" => username, :password => 'secret' })
         end
 
-        #Accepts route argument
-        #Default - 'logout_url'
-        def logout_user(route = nil)
+        #Accepts route and HTTP method arguments
+        #Default - 'logout_url' and GET
+        def logout_user(route = nil, http_method = :get)
           route ||= logout_url
-          page.driver.get(route)
+          page.driver.send(http_method, route)
         end
       end
     end
