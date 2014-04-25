12c8dd5ee903ae34be8f58895887ad86b1af73bc
Gustavo Chain <gchain@gmail.com>
2014-04-25 09:29:59 -0300

    Fix scope shared example indentation

--- spec/shared_examples/controller_scope_shared_examples.rb
+++ spec/shared_examples/controller_scope_shared_examples.rb
@@ -1,20 +1,20 @@
 shared_examples_for 'scoped' do
-	before do
-		User.scope :without_username, -> {User.where username: ''}
-		sorcery_model_property_set(:scope, :without_username)
-	end
+  before do
+    User.scope :without_username, -> {User.where username: ''}
+    sorcery_model_property_set(:scope, :without_username)
+  end
 
-	it "login(username,password) should look for user within specified scope. User should not be found" do
-		create_new_user({:username => "GIZMO1", :email => 'bla1@bla.com', :password => 'secret1'})
-		get :test_login, :email => 'bla1@bla.com', :password => 'secret1'
-		assigns[:user].should be_nil
-		session[:user_id].should be_nil
-	end
+  it "login(username,password) should look for user within specified scope. User should not be found" do
+    create_new_user({:username => "GIZMO1", :email => 'bla1@bla.com', :password => 'secret1'})
+    get :test_login, :email => 'bla1@bla.com', :password => 'secret1'
+    assigns[:user].should be_nil
+    session[:user_id].should be_nil
+  end
 
-	it "login(username,password) should look for user within specified scope. User should be found" do
-		create_new_user({:username => "", :email => 'bla1@bla.com', :password => 'secret1'})
-		get :test_login, :email => 'bla1@bla.com', :password => 'secret1'
-		assigns[:user].should == @user
-		session[:user_id].should == @user.id
-	end
+  it "login(username,password) should look for user within specified scope. User should be found" do
+    create_new_user({:username => "", :email => 'bla1@bla.com', :password => 'secret1'})
+    get :test_login, :email => 'bla1@bla.com', :password => 'secret1'
+    assigns[:user].should == @user
+    session[:user_id].should == @user.id
+  end
 end
