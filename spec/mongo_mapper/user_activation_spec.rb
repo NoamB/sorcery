b6f02768316bb137e38d347bc68228916981f84c
Arnvald <arnvald.to@gmail.com>
2014-06-06 16:31:25 +0700

    update test descriptions for mongo_mapper

--- spec/mongo_mapper/user_activation_spec.rb
+++ spec/mongo_mapper/user_activation_spec.rb
@@ -1,9 +1,8 @@
 require 'spec_helper'
-
 require 'rails_app/app/mailers/sorcery_mailer'
 require 'shared_examples/user_activation_shared_examples'
 
-describe "User with activation submodule", :mongo_mapper => true do
+describe User, "with activation submodule", :mongo_mapper => true do
 
   it_behaves_like "rails_3_activation_model"
 
