84c6f4dff739f0de97e7bad29d3fd437aa8056be
Arnvald <arnvald.to@gmail.com>
2014-07-16 10:18:32 +0800

    Store user_id in session as string
    Currently we store in session `user.id` which in some cases is integer, but in
    some cases (like Mongoid) is an object.
    What's the problem with storing object? First thing, it might break
    compatibility between different ORM versions (Mongoid changed class of this
    object from v2 to v3). Second, the bigger issue, is that new serializer in Rails
    uses JSON instead of YAML, so it can store only selected types, and it parses
    custom objects incorrectly, which may break the authentication feature.
    
    This commit changes the way we store user id to plain string. This will make it
    being stored correctly no matter what serializer is used.

--- lib/sorcery/controller/submodules/remember_me.rb
+++ lib/sorcery/controller/submodules/remember_me.rb
@@ -38,7 +38,7 @@ module Sorcery
           # Override.
           # logins a user instance, and optionally remembers him.
           def auto_login(user, should_remember = false)
-            session[:user_id] = user.id
+            session[:user_id] = user.id.to_s
             @current_user = user
             remember_me! if should_remember
           end
@@ -58,7 +58,7 @@ module Sorcery
             user = cookies.signed[:remember_me_token] && user_class.sorcery_adapter.find_by_remember_me_token(cookies.signed[:remember_me_token])
             if user && user.has_remember_me_token?
               set_remember_me_cookie!(user)
-              session[:user_id] = user.id
+              session[:user_id] = user.id.to_s
               @current_user = user
             else
               @current_user = false
