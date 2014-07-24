9907cd45b9429c545160597b030914256dd00efc
palkan <dementiev@webils.ru>
2014-07-24 19:01:36 +0400

    remove useless failing spec

--- spec/mongoid/user_password_validation_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-require 'shared_examples/user_password_validation_shared_examples'
-
-describe User, "with password_validation submodule", :mongoid => true do
-
-  it_behaves_like "rails_3_password_validation_model"
-
-end
