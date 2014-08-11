ebda69d81d16f04f9975db4365ebb4a630d56547
Sasha A <elequtree@gmail.com>
2014-08-11 18:39:20 +0700

    Removed generator_spec.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,8 +14,6 @@ require 'rspec'
 require 'rails/all'
 require 'rspec/rails'
 require 'timecop'
-# require 'generator_spec'
-# require 'generator_spec/generator_example_group'
 
 def setup_orm; end
 def teardown_orm; end
@@ -44,6 +42,4 @@ RSpec.configure do |config|
 
   config.include ::Sorcery::TestHelpers::Internal
   config.include ::Sorcery::TestHelpers::Internal::Rails
-
-  # config.include ::GeneratorSpec::GeneratorExampleGroup, :type => :generator
 end
