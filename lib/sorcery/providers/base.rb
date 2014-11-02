70494f4e24483d14925e6651c8a1f1b01deceb90
Ty Rauber <tyrauber@mac.com>
2014-11-02 12:28:23 -0800

    feature/oauth_token :: dup hash in providers/base auth_hash method so as to prevent mutation.

--- lib/sorcery/providers/base.rb
+++ lib/sorcery/providers/base.rb
@@ -14,13 +14,14 @@ module Sorcery
       end
 
       def auth_hash(access_token, hash={})
+        token_hash = hash.dup
         if access_token
-          hash[:token] = access_token.token if access_token.respond_to?(:token)
-          hash[:refresh_token] = access_token.refresh_token if access_token.respond_to?(:refresh_token)
-          hash[:expires_at] = access_token.expires_at if access_token.respond_to?(:expires_at)
-          hash[:expires_in] = access_token.expires_at if access_token.respond_to?(:expires_in)
+          token_hash[:token] = access_token.token if access_token.respond_to?(:token)
+          token_hash[:refresh_token] = access_token.refresh_token if access_token.respond_to?(:refresh_token)
+          token_hash[:expires_at] = access_token.expires_at if access_token.respond_to?(:expires_at)
+          token_hash[:expires_in] = access_token.expires_at if access_token.respond_to?(:expires_in)
         end
-        hash
+        token_hash
       end
 
       def self.name
