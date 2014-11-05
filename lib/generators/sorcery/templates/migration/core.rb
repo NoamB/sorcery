9722ed79b72e784c8af8cc9c7dd22be2d213e137
Felix Borzik <borzik1992@gmail.com>
2014-11-05 23:47:47 +0200

    User.create_from_provider with empty password fix

--- lib/generators/sorcery/templates/migration/core.rb
+++ lib/generators/sorcery/templates/migration/core.rb
@@ -2,8 +2,8 @@ class SorceryCore < ActiveRecord::Migration
   def change
     create_table :<%= model_class_name.tableize %> do |t|
       t.string :email,            :null => false
-      t.string :crypted_password, :null => false
-      t.string :salt,             :null => false
+      t.string :crypted_password
+      t.string :salt
 
       t.timestamps
     end
--- spec/rails_app/db/migrate/core/20101224223620_create_users.rb
+++ spec/rails_app/db/migrate/core/20101224223620_create_users.rb
@@ -3,8 +3,8 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :username,         :null => false
       t.string :email,            :default => nil
-      t.string :crypted_password, :default => nil
-      t.string :salt,             :default => nil
+      t.string :crypted_password
+      t.string :salt
 
       t.timestamps
     end
