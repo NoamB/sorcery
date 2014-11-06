b033d6fdcbeb442da4147a2d70a7cba3d30b8edb
Arnvald <arnvald.to@gmail.com>
2014-11-06 20:42:45 +0000

    Do not copy auth hash if there is no need
    If `access_token` is nil, we do not want to modify the hash, therefore there's
    no need to copy it

--- lib/sorcery/providers/base.rb
+++ lib/sorcery/providers/base.rb
@@ -14,13 +14,13 @@ module Sorcery
       end
 
       def auth_hash(access_token, hash={})
+        return hash if access_token.nil?
+
         token_hash = hash.dup
-        if access_token
-          token_hash[:token] = access_token.token if access_token.respond_to?(:token)
-          token_hash[:refresh_token] = access_token.refresh_token if access_token.respond_to?(:refresh_token)
-          token_hash[:expires_at] = access_token.expires_at if access_token.respond_to?(:expires_at)
-          token_hash[:expires_in] = access_token.expires_at if access_token.respond_to?(:expires_in)
-        end
+        token_hash[:token] = access_token.token if access_token.respond_to?(:token)
+        token_hash[:refresh_token] = access_token.refresh_token if access_token.respond_to?(:refresh_token)
+        token_hash[:expires_at] = access_token.expires_at if access_token.respond_to?(:expires_at)
+        token_hash[:expires_in] = access_token.expires_at if access_token.respond_to?(:expires_in)
         token_hash
       end
 
