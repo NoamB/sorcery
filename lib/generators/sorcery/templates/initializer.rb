f6a46e08709ea004a7bdc7271e4891c4b4215032
DMips <dominika.mips@amberbit.com>
2015-06-24 10:22:57 +0200

    Add google scope to initializer
    Recently I wanted to change scope in google and I spent some time on it so maybe if we put it in initializer somebody else won't have this problem :)

--- lib/generators/sorcery/templates/initializer.rb
+++ lib/generators/sorcery/templates/initializer.rb
@@ -132,6 +132,7 @@ Rails.application.config.sorcery.configure do |config|
   # config.google.secret = ""
   # config.google.callback_url = "http://0.0.0.0:3000/oauth/callback?provider=google"
   # config.google.user_info_mapping = {:email => "email", :username => "name"}
+  # config.google.scope = "https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile"
   #
   # config.vk.key = ""
   # config.vk.secret = ""
