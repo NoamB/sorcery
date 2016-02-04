35189bfd5c18b54fed9f52773d2ff71d84841db4
Rudolph-Miller <chopsticks.tk.ppfm@gmail.com>
2016-02-04 11:59:01 +0900

    Use cunstom remembe_me_token_key

--- lib/sorcery/controller/submodules/remember_me.rb
+++ lib/sorcery/controller/submodules/remember_me.rb
@@ -32,13 +32,13 @@ module Sorcery
           # Clears the cookie, and depending on the value of remember_me_token_persist_globally, may clear the token value.
           def forget_me!
             current_user.forget_me!
-            cookies.delete(:remember_me_token, :domain => Config.cookie_domain)
+            cookies.delete(remember_me_token_key, :domain => Config.cookie_domain)
           end
 
           # Clears the cookie, and clears the token value.
           def force_forget_me!
             current_user.force_forget_me!
-            cookies.delete(:remember_me_token, :domain => Config.cookie_domain)
+            cookies.delete(remember_me_token_key, :domain => Config.cookie_domain)
           end
 
           # Override.
@@ -61,7 +61,7 @@ module Sorcery
           # and logs the user in if found.
           # Runs as a login source. See 'current_user' method for how it is used.
           def login_from_cookie
-            user = cookies.signed[:remember_me_token] && user_class.sorcery_adapter.find_by_remember_me_token(cookies.signed[:remember_me_token])
+            user = cookies.signed[remember_me_token_key] && user_class.sorcery_adapter.find_by_remember_me_token(cookies.signed[remember_me_token_key])
             if user && user.has_remember_me_token?
               set_remember_me_cookie!(user)
               session[:user_id] = user.id.to_s
@@ -72,15 +72,18 @@ module Sorcery
           end
 
           def set_remember_me_cookie!(user)
-            cookies.signed[:remember_me_token] = {
+            cookies.signed[remember_me_token_key] = {
               :value => user.send(user.sorcery_config.remember_me_token_attribute_name),
               :expires => user.send(user.sorcery_config.remember_me_token_expires_at_attribute_name),
               :httponly => Config.remember_me_httponly,
               :domain => Config.cookie_domain
             }
           end
-        end
 
+          def remember_me_token_key
+            Config.remember_me_token_key
+          end
+        end
       end
     end
   end
